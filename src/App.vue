b96dac42d1a588d32b8ef8e474140e3c19a1d415
6A6A3uHA <markisa130106@gmail.com>
2026-05-05 18:15:54 +0300

    Сделал предполагаемую структуру

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <script setup>
 import { RouterLink } from 'vue-router';
 import home from './composables/home.vue';
+import Game1 from './components/Game1.vue';
 
 
 </script>
 
 <template>
-    <RouterLink :to="{name: home}">1</RouterLink>
-    <RouterLink :to="{name: nothome}">2</RouterLink>
-    <RouterView/>
+    <header>
+        <RouterLink :to="{ name: home }">1</RouterLink>
+        <RouterLink :to="{ name: Game1 }">2</RouterLink>
+    </header>
+    <section v-if="home">
+        <!--Карточки игр, отцентровать и т д-->
+    </section>
+    <section v-if="nothome">
+        <RouterView/>
+    </section>
 </template>
 
 <style scoped>
--- src/route.js
+++ src/route.js
@@ -1,13 +1,14 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import home from './composables/home.vue'
-import Nothome from './composables/nothome.vue'
+import Game1 from './components/Game1.vue'
 
 const routes = [
-    {path:"/", component:home, name:"home"},
-    {path:"/not-home", component: Nothome, name:"nothome"},
-    {path:"/...", component:'', name:""},
-    {path:"/....", component:'', name:""},
-    {path:"/.....", component:'', name:""}
+    { path: "/", component: home, name: "home" },
+    { path: "/game1", component: Game1, name: "game1" },
+    { path: "/game2", component: Game2, name: "game2" },
+    { path: "/game3", component: Game3, name: "game3" },
+    { path: "/game4", component: Game4, name: "game4" },
+    {path:"/leaderboard", component: leaderboard, name:"leaderboard", child},
 ]
 
 export const router = createRouter({
