8250d8af2b746dd280976433ef1e3d5b51704749
6A6A3uHA <markisa130106@gmail.com>
2026-05-05 22:34:41 +0300

    Добавление поддержки typescript

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,11 @@
 import { RouterLink } from 'vue-router';
 import home from './composables/home.vue';
 
-
 </script>
 
 <template>
     <header>
         <RouterLink :to="{ name: home }">1</RouterLink>
-        <RouterLink :to="{ name: 'game1' }">2</RouterLink>
     </header>
     <section>
         <RouterView/>
