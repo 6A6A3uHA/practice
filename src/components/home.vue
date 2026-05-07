447771e4583ef1215830b395a27f1fcfb0fd1f72
6A6A3uHA <markisa130106@gmail.com>
2026-05-07 17:03:23 +0300

    Перевел все файлы в ts

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,9 +1,12 @@
-<script setup>
-
+<script setup lang="ts">
+    import { ref } from 'vue';
+    import { choosed_game_type } from '../composables/useType';
+    /**
+     * По умолчанию 0
+     */
+    const choose_game = ref<choosed_game_type | 0>(0)
 </script>
 <template>
 
 </template>
-<style scoped>
-
-</style>
+<style scoped></style>
