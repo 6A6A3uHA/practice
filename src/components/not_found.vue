dafe05170ec80af56623443e4853624b0e58f376
6A6A3uHA <markisa130106@gmail.com>
2026-05-08 03:00:27 +0300

    Нужно дописать

--- src/components/not_found.vue
+++ src/components/not_found.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-
+    import { useRoute } from 'vue-router';
+    import { router } from '../route';
+    if (useRoute().path != '/not_found') {
+        router.push({ path: '/not_found' })
+    }
 </script>
 <template>
-
+    <h1>Страницы не существует</h1>
 </template>
 <style scoped></style>
