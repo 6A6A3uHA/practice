5fe395812860090fdf6fe235c922303b6ab59d87
6A6A3uHA <markisa130106@gmail.com>
2026-05-08 03:02:49 +0300

    Экспортируемая функция для переключения маршрута в коде и темплейте, дает подсказки при написании имени

--- src/composables/useNavigate.ts
+++ src/composables/useNavigate.ts
@@ -1,1 +1,13 @@
-import { useRouter } from "vue-router";
+import { router } from "../route"
+
+function Navigate(name: 'board', numgame: 1 | 2 | 3 | 4): void
+function Navigate(name: 'home' | 'game1' | 'game2' | 'game3' | 'game4' | 'leaderboard' | '404'): void
+
+function Navigate(name: string, numgame?: number): void {
+    if (numgame) {
+        router.push({ name: name, params: { numgame: String(numgame) } })
+    }
+    else router.push({ name: name })
+}
+
+export default { Navigate }
