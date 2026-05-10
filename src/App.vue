47d924435a70eaa637b7cdf0aebe3d7ac423f1b7
6A6A3uHA <markisa130106@gmail.com>
2026-05-10 10:19:18 +0300

    Убрал debug в board.vue, добавил поддержку телефонов для header и game1 создал объекты для игры в пинг понг, отключил таблицу рейтинга по умолчанию

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,56 @@
 <script setup lang="ts">
-    import { computed } from 'vue';
+    import { computed, onMounted, ref, watch } from 'vue';
     import { RouterLink } from 'vue-router';
     import { useRoute } from 'vue-router';
-    const isHome = computed(() => useRoute().name === 'home')
+
+    const route = useRoute()
+    const isHome = computed(() => route.name === 'home')
     const nav = computed(() => {
         return isHome.value ? 'end' : 'space-around'
     })
+    const normal_size = ref<boolean>(true)
+    const width_header = ref<number | null>(null)
+    watch(width_header, () => {
 
+        if (!width_header.value || width_header.value < 1300) normal_size.value = false
+
+        else normal_size.value = true
+    })
+
+    const getDynamicSize = new ResizeObserver((list_of_elements) => {
+        const header = list_of_elements[0]
+        const { width } = header?.contentRect as DOMRectReadOnly
+        width_header.value = width
+    })
+
+    const open_narrow_navigation = ref<boolean>(false)
+
+    const link_header = ref<HTMLElement | null>(null)
+
+    onMounted(() => {
+        if (link_header.value) getDynamicSize.observe(link_header.value)
+    })
+
+
+    const height = ref<string>('0')
+
+    watch(open_narrow_navigation, () => {
+        if (open_narrow_navigation.value) height.value = '100vh'
+        else height.value = '0'
+    })
+    watch(route, () => {
+        open_narrow_navigation.value = false
+    })
 </script>
 
 <template>
-    <header>
+    <header ref="link_header">
         <div class="left_header">
             <RouterLink :to="{ name: 'home' }" title="Вернуться на главную">
                 <img src="./assets/vue.svg" alt="logo or smth">
             </RouterLink>
         </div>
-        <div class="navigation">
+        <div class="navigation" v-if="normal_size">
             <RouterLink :to="{ name: 'board', params: { numgame: 1 } }">
                 Рейтинг игроков
             </RouterLink>
@@ -33,8 +67,37 @@
                 Название игры4
             </RouterLink>
         </div>
+        <div v-else class="mobile_navigation">
+            <button class="icon_narrow_nav" @click="open_narrow_navigation = !open_narrow_navigation">
+                {{ !open_narrow_navigation ? '&#8801;' : '&#9662;' }}
+            </button>
+        </div>
     </header>
     <main>
+        <nav class="narrow_navigation" @click="open_narrow_navigation = false">
+            <div class="nav_block">
+                <RouterLink :to="{ name: 'board', params: { numgame: 1 } }"
+                    :class="{ narrow_navigation_selected: route.name == 'board' ? true : false }">
+                    Рейтинг игроков
+                </RouterLink>
+                <RouterLink :to="{ name: 'game1' }"
+                    :class="{ narrow_navigation_selected: route.name == 'game1' ? true : false }">
+                    Пинг-понг
+                </RouterLink>
+                <RouterLink :to="{ name: 'game2' }"
+                    :class="{ narrow_navigation_selected: route.name == 'game2' ? true : false }">
+                    Название игры2
+                </RouterLink>
+                <RouterLink :to="{ name: 'game3' }"
+                    :class="{ narrow_navigation_selected: route.name == 'game3' ? true : false }">
+                    Назыание игры3
+                </RouterLink>
+                <RouterLink :to="{ name: 'game4' }"
+                    :class="{ narrow_navigation_selected: route.name == 'game4' ? true : false }">
+                    Название игры4
+                </RouterLink>
+            </div>
+        </nav>
         <RouterView />
     </main>
     <footer>
@@ -53,8 +116,8 @@
         /**Никогда ничем не перекроется */
         left: 0;
         top: 0;
-        width: 100%;
-        height: auto;
+        right: 0;
+        height: 30px;
         display: flex;
         justify-content: space-evenly;
         align-items: center;
@@ -70,21 +133,68 @@
         width: 50%;
     }
 
+    .icon_narrow_nav {
+        margin: 0px;
+        padding: 0px;
+        font-size: 30px;
+        cursor: pointer;
+        width: 40px;
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
     main {
+        min-height: 40vh;
+        height: max-content;
         height: 100%;
         width: 100%;
-        margin-top: 75px;
+        margin-top: 70px;
         padding: 0px;
         position: relative;
         flex-grow: 1;
         display: flex;
         flex-flow: column nowrap;
         align-items: center;
+    }
+
+    .narrow_navigation {
+        height: v-bind(height);
+        width: 100%;
         flex: 1;
+        position: fixed;
+        z-index: 9998;
+        top: 0;
+        left: 0;
+        right: 0;
+        transition: height 400ms ease-in-out;
+        overflow: hidden;
+        background-color: rgb(32, 33, 34);
+        display: flex;
+        margin-top: 70px;
+        align-items: start;
+        justify-content: center;
+    }
+
+    .nav_block {
+        position: sticky;
+        display: flex;
+        flex-flow: column nowrap;
+        height: min-content;
+        width: fit-content;
+        justify-content: space-evenly;
+    }
+
+    .narrow_navigation_selected {
+        color: rgb(245, 243, 112);
     }
 
     footer {
+        z-index: 9999;
+        min-height: 100px;
         margin-top: 80px;
-        margin-bottom: 80px;
+        padding-bottom: 40px;
+        background-color: rgb(24, 24, 24);
     }
 </style>
